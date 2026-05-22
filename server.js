11e8d0300d817645e57cf27ef9ac06b70da2fb8b
Kareem Khalifa <karemkhalifaa@gmail.com>
2026-05-22 11:38:14 +0300

    fix: use path.join for db.json location

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('src/db.json');
+const router = jsonServer.router(path.join(__dirname, 'src', 'db.json'));
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
